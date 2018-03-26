b09a1af559ca6227466c6fefb84d7988cb5ecc0d
Brad Solomon <brad.solomon.1124@gmail.com>
2018-03-26 08:29:34 -0400

    remove .sh

--- src/cpaws.sh
+++
@@ -1,5 +0,0 @@
-scp -rp /Users/brad/Scripts/python/metis/metisgh/projects/kojak/nlsy79/src/ brad@$(echo $(ipaws) | sed -e 's/^"//' -e 's/"$//'):/home/brad/nlsy79/src/
-scp -rp /Users/brad/Scripts/python/metis/metisgh/projects/kojak/nlsy79/downloads/ brad@$(echo $(ipaws) | sed -e 's/^"//' -e 's/"$//'):/home/brad/nlsy79/downloads/
-scp -rp /Users/brad/Scripts/python/metis/metisgh/projects/kojak/nlsy79/plots/ brad@$(echo $(ipaws) | sed -e 's/^"//' -e 's/"$//'):/home/brad/nlsy79/plots/
-scp -rp /Users/brad/Scripts/python/metis/metisgh/projects/kojak/nlsy79/data/ brad@$(echo $(ipaws) | sed -e 's/^"//' -e 's/"$//'):/home/brad/nlsy79/data/
-scp -rp /Users/brad/Scripts/python/metis/metisgh/projects/kojak/nlsy79/marstat/ brad@$(echo $(ipaws) | sed -e 's/^"//' -e 's/"$//'):/home/brad/nlsy79/marstat/
